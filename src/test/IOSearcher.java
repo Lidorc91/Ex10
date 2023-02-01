ff0a06da5b9bfbbd22c9aa93219e1ffdffe1bae9
Lidorc91 <lidorc91@gmail.com>
2023-02-01 23:44:21 +0200

    IOSearcher Update

--- src/test/IOSearcher.java
+++ src/test/IOSearcher.java
@@ -1,18 +1,54 @@
 package test;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
-public class IOSearcher implements FileSearcher{
+public class IOSearcher implements FileSearcher {
+    volatile boolean stopSearch;
+    Thread t;
 
     @Override
     public boolean search(String word, String... fileNames) {
-        // TODO Auto-generated method stub
+        t = new Thread(searchFiles(word, fileNames));
+        boolean b = t.start();
+    }
+
+    private boolean searchFiles(String word,String... fileNames){
+        String searchedWord;
+        for (String name : fileNames) {
+            try {
+                FileReader file = new FileReader(name);
+                Scanner s = new Scanner(file);
+                try {
+                    while (s.hasNext() && !stopSearch) {
+                        searchedWord = s.next();
+                        String[] splitWord = searchedWord.split(",|\\.");
+                        for (String w : splitWord) {
+                            if (w == word) {
+                                return true;
+                            }
+                        }
+                    }
+                } finally {
+                    s.close();
+                    try {
+                        file.close();
+                    } catch (IOException e) {
+                        return false;
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                return false;
+            }            
+        }
         return false;
     }
 
     @Override
     public void stop() {
-        // TODO Auto-generated method stub
-        
+        stopSearch = true;
     }
 
 }
